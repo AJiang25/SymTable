0bcd6002dbd8794b4d196a1347a40aa0ddb10491
Arnold Jiang <aj9183@princeton.edu>
2023-03-07 21:19:48 -0500

    fixed a typo and moved a static function

--- symtablehash.c
+++ symtablehash.c
@@ -35,6 +35,18 @@ struct Bind {
     struct Bind *next;
 };
 
+/* Return a hash code for pcKey that is between 0 and uBucketCount-1,
+   inclusive. */
+static size_t SymTable_hash(const char *pcKey, size_t uBucketCount)
+{
+   const size_t HASH_MULTIPLIER = 65599;
+   size_t u;
+   size_t uHash = 0;
+   assert(pcKey != NULL);
+   for (u = 0; pcKey[u] != '\0'; u++)
+      uHash = uHash * HASH_MULTIPLIER + (size_t)pcKey[u];
+   return uHash % uBucketCount;
+}
 
 static size_t Expand(SymTable_T oSymTable) {
     /*last array index in auBucketCounts[]*/
@@ -59,7 +71,7 @@ static size_t Expand(SymTable_T oSymTable) {
     }
 
     /*callocs the buckets based on auBucketCounts*/
-    oSymTable->buckets = (struct bind*)calloc(auBucketCounts[i], 
+    oSymTable->buckets = (struct Bind*)calloc(auBucketCounts[i], 
         sizeof(struct Bind*));
 
     /*checks if successful*/
@@ -87,19 +99,6 @@ static size_t Expand(SymTable_T oSymTable) {
     /*Frees the old array & sets the pointer ot the new array*/
 
     return auBucketCounts[i];
-}
-
-/* Return a hash code for pcKey that is between 0 and uBucketCount-1,
-   inclusive. */
-static size_t SymTable_hash(const char *pcKey, size_t uBucketCount)
-{
-   const size_t HASH_MULTIPLIER = 65599;
-   size_t u;
-   size_t uHash = 0;
-   assert(pcKey != NULL);
-   for (u = 0; pcKey[u] != '\0'; u++)
-      uHash = uHash * HASH_MULTIPLIER + (size_t)pcKey[u];
-   return uHash % uBucketCount;
 }
 
 SymTable_T SymTable_new(void) {
